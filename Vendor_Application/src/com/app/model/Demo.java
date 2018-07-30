f7d11aea2a6e14a320949e4f9f038ce1a277db8a
madhusmita <infosmita89@gmail.com>
2018-07-30 14:50:52 +0530

    3rd change

--- Vendor_Application/src/com/app/model/Demo.java
+++ Vendor_Application/src/com/app/model/Demo.java
@@ -3,8 +3,17 @@ package com.app.model;
 public class Demo {
 
 	private int name ;
+	private String nameww;;
 
 	
+	public String getNameww() {
+		return nameww;
+	}
+
+	public void setNameww(String nameww) {
+		this.nameww = nameww;
+	}
+
 	public Demo() {
 		super();
 	}
