2de4d9758997254b8ca9c7f27eb39d5d2eeb5840
madhusmita <infosmita89@gmail.com>
2018-07-30 14:45:57 +0530

    second commit

---
+++ Vendor_Application/src/com/app/model/Demo.java
@@ -0,0 +1,20 @@
+package com.app.model;
+
+public class Demo {
+
+	private int name ;
+
+	
+	public Demo() {
+		super();
+	}
+
+	public int getName() {
+		return name;
+	}
+
+	public void setName(int name) {
+		this.name = name;
+	}
+	
+}
